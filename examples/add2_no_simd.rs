80d4f22d27ab0da1acef6bed7b885325ff758f99
Sven Nilsen <bvssvni@gmail.com>
2014-10-13 13:31:18 +0200

    Renamed files

--- examples/add2_no_simd.rs
+++ examples/add2_no_simd.rs
@@ -3,7 +3,7 @@
 extern crate vec;
 
 fn main() {
-    println!("ADD NO SIMD");
+    println!("ADD2 NO SIMD");
     let mut sum = (0.0f64, 0.0f64);
     for _ in range(0u, vec::TEST_ITERATIONS) {
         sum = (sum.0 + 1.0f64, sum.1 + 0.3f64);
--- examples/add2_simd.rs
+++ examples/add2_simd.rs
@@ -2,7 +2,7 @@ extern crate vec;
 
 fn main() {
     use std::simd::f64x2;
-    println!("ADD SIMD");
+    println!("ADD2 SIMD");
     let mut sum = f64x2(0.0, 0.0);
     for _ in range(0u, vec::TEST_ITERATIONS) {
         sum = sum + f64x2(1.0, 0.3);
